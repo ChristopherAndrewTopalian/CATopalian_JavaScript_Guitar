ae2d35759c68527cd4c061bb478856eaebb6ba7c
Christopher Andrew Topalian <scriptingdata@gmail.com>
2025-10-12 10:23:19 -0400

    update

--- src/js/3data/scales/locrian.js
+++ src/js/3data/scales/locrian.js
@@ -10,17 +10,17 @@ let cLocrian = ["c", "csharp", "dsharp", "f", "fsharp", "gsharp", "asharp"];
 
 let cSharpLocrian = ["csharp", "d", "e", "fsharp", "g", "a", "b"];
 
-let dLocrian = ["d", "eb", "f", "g", "ab", "bb", "c"];
+let dLocrian = ["d", "dsharp", "f", "g", "gsharp", "asharp", "c"];
 
 let dSharpLocrian = ["dsharp", "e", "fsharp", "gsharp", "a", "b", "csharp"];
 
 let eLocrian = ["e", "f", "g", "a", "bb", "b", "d"];
 
-let fLocrian = ["f", "gb", "gsharp", "bb", "cb", "db", "eb"];
+let fLocrian = ["f", "fsharp", "gsharp", "asharp", "asharp", "csharp", "dsharp"];
 
 let fSharpLocrian = ["fsharp", "g", "a", "b", "c", "d", "e"];
 
-let gLocrian = ["g", "ab", "bb", "c", "db", "dsharp", "f"];
+let gLocrian = ["g", "gsharp", "asharp", "c", "csharp", "dsharp", "f"];
 
 let gSharpLocrian = ["gsharp", "a", "b", "csharp", "d", "e", "fsharp"];
 
