3dd7274b8d7e627253b7d77c7a7fcc0ecbbc48e1
Christopher Andrew Topalian <scriptingdata@gmail.com>
2025-10-12 01:54:11 -0400

    update

---
+++ src/js/3data/scales/majorDiatonic.js
@@ -0,0 +1,25 @@
+// majorDiatonicScales.js
+
+let aMajorScale = [ "a", "b", "csharp", "d", "e", "fsharp", "gsharp" ];
+
+let asharpMajorScale = ["asharp", "c", "csharp", "dsharp", "f", "fsharp", "gsharp"];
+
+let bMajorScale = [ "b", "csharp", "dsharp", "e", "fsharp", "gsharp", "asharp" ];
+
+let cMajorScale = [ "c", "d", "e", "f", "g", "a", "b" ];
+
+let csharpMajorScale = [ "csharp", "dsharp", "e", "fsharp", "gsharp", "asharp", "b" ];
+
+let dMajorScale = [ "d", "e", "fsharp", "g", "a", "b", "csharp" ];
+
+let dsharpMajorScale = [ "dsharp", "f", "fsharp", "gsharp", "asharp", "c", "csharp" ];
+
+let eMajorScale = [ "e", "fsharp", "gsharp", "a", "b", "csharp", "dsharp" ];
+
+let fMajorScale = [ "f", "g", "a", "asharp", "c", "d", "e" ];
+
+let fsharpMajorScale = [ "fsharp", "gsharp", "asharp", "b", "csharp", "dsharp", "f" ];
+
+let gMajorScale = [ "g", "a", "b", "c", "d", "e", "fsharp" ];
+
+let gsharpMajorScale = [ "gsharp", "asharp", "b", "csharp", "dsharp", "f", "fsharp" ];
--- src/js/color/colorTheScale.js
+++ src/js/color/colorTheScale.js
@@ -1,45 +1,5 @@
 // colorTheScale.js
 
-let aMajorScale = [ "a", "b", "csharp", "d", "e", "fsharp", "gsharp" ];
-
-let asharpMajorScale = ["asharp", "c", "csharp", "dsharp", "f", "fsharp", "gsharp"];
-
-let bMajorScale = [ "b", "csharp", "dsharp", "e", "fsharp", "gsharp", "asharp" ];
-
-let cMajorScale = [ "c", "d", "e", "f", "g", "a", "b" ];
-
-let csharpMajorScale = [ "csharp", "dsharp", "e", "fsharp", "gsharp", "asharp", "b" ];
-
-let dMajorScale = [ "d", "e", "fsharp", "g", "a", "b", "csharp" ];
-
-let dsharpMajorScale = [ "dsharp", "f", "fsharp", "gsharp", "asharp", "c", "csharp" ];
-
-let eMajorScale = [ "e", "fsharp", "gsharp", "a", "b", "csharp", "dsharp" ];
-
-let fMajorScale = [ "f", "g", "a", "asharp", "c", "d", "e" ];
-
-let fsharpMajorScale = [ "fsharp", "gsharp", "asharp", "b", "csharp", "dsharp", "f" ];
-
-let gMajorScale = [ "g", "a", "b", "c", "d", "e", "fsharp" ];
-
-let gsharpMajorScale = [ "gsharp", "asharp", "b", "csharp", "dsharp", "f", "fsharp" ];
-
-//-//
-
-// Minor Diatonic Scales
-let aMinorScale = ["a", "b", "c", "d", "e", "f", "g"];
-let aSharpMinorScale = ["asharp", "b", "csharp", "dsharp", "f", "fsharp", "gsharp"];
-let bMinorScale = ["b", "csharp", "d", "e", "fsharp", "g", "a"];
-let cMinorScale = ["c", "d", "dsharp", "f", "g", "gsharp", "asharp"];
-let cSharpMinorScale = ["csharp", "dsharp", "e", "fsharp", "gsharp", "a", "b"];
-let dMinorScale = ["d", "e", "f", "g", "a", "asharp", "c"];
-let dSharpMinorScale = ["dsharp", "f", "fsharp", "gsharp", "asharp", "b", "csharp"];
-let eMinorScale = ["e", "fsharp", "g", "a", "b", "c", "d"];
-let fMinorScale = ["f", "g", "gsharp", "asharp", "c", "d", "dsharp"];
-let fSharpMinorScale = ["fsharp", "gsharp", "a", "b", "csharp", "d", "e"];
-let gMinorScale = ["g", "a", "asharp", "c", "d", "dsharp", "f"];
-let gSharpMinorScale = ["gsharp", "asharp", "b", "csharp", "dsharp", "e", "fsharp"];
-
 function colorTheScale(whichScale)
 {
     ge('chosenScale').textContent = whichScale;
